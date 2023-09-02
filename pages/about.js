16a8e4a298c9e8e6e677f808083e1ee7f2a041d3
Danniel-09 <142106900+Danniel-09@users.noreply.github.com>
2023-09-02 16:01:08 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <p>This is the about page of our website.</p>
+      <p>Hello, I am Danniel Jose C. Prado. I created this website for the purpose of learning and improving my coding skills.</p>
     </div>
   );
 };
